c14515b80a64f473f2ebc313bf69906e864da82b
kdaud <dkakumirizii@gmail.com>
2020-01-13 21:02:50 +0300

    codes

--- src/com/opencode/MainT.java
+++ src/com/opencode/MainT.java
@@ -1,6 +1,9 @@
 package com.opencode;
 
 public class MainT {
+    static {
+        System.out.println("I Love this thing of coding and i want to be a software engineer some day");
+    }
     public static void main(String[] args) {
 //        Mentors obj = new Mentors();
 //        obj.table();
