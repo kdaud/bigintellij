9679e859e61e72bb30b6fab8904bff29e00dfd0b
kdaud <dkakumirizii@gmail.com>
2020-01-13 21:11:20 +0300

    stuffs

--- src/com/stacks/Pupils.java
+++ src/com/stacks/Pupils.java
@@ -1,5 +1,16 @@
 package com.stacks;
 
+import java.util.Stack;
+
 public class Pupils {
-    
+    private Stack<String> students = new Stack<>();
+    public void mycall(){
+        students.push("Gama");
+        students.push("Godfrey");
+        students.push("Cliff");
+        students.push("Aaron");
+    for(String ss: students){
+        System.out.println(ss);
+    }
+    }
 }
