8c91a271e3dab62a4c4b4e62d6366194278f4016
kdaud <dkakumirizii@gmail.com>
2020-01-13 21:04:50 +0300

    files

--- src/com/opencode/MainT.java
+++ src/com/opencode/MainT.java
@@ -4,6 +4,17 @@ public class MainT {
     static {
         System.out.println("I Love this thing of coding and i want to be a software engineer some day");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
     public static void main(String[] args) {
 //        Mentors obj = new Mentors();
 //        obj.table();
@@ -14,4 +25,5 @@ public class MainT {
     public static String mygrade(){
         return "Mysql is loading";
     }
+    //my idea is really interesting to write codes
 }
