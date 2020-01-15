a59887f49858abd4a1943524105bac162d820817
kdaud <dkakumirizii@gmail.com>
2020-01-15 03:03:02 +0300

    codes

--- src/com/stacks/Main.java
+++ src/com/stacks/Main.java
@@ -1,12 +1,16 @@
 package com.stacks;
 
 public class Main {
+    static {
+        System.out.println("My coding journey has been blessed by God");
+    }
     public static void main(String[] args) {
         //new Pupils().mycall();
         new Bigcode().software();
     }
     //this block executes first than other block codes
     static{
+
         System.out.println("My static block in progress");
     }
 }
