5bc193a7dc7085cef8c8184a257b469dd81f14c0
kdaud <dkakumirizii@gmail.com>
2020-01-15 20:31:06 +0300

    codes

---
+++ src/org/software/BigNote.java
@@ -0,0 +1,15 @@
+package org.software;
+
+import java.util.Random;
+
+;
+
+public class BigNote {
+    private Random mycodes(){
+        Random obj = new Random();
+        for(int i=10; i<20;i++){
+            System.out.println(obj.doubles());
+        }
+        return  obj;
+    }
+}
---
+++ src/org/software/Main.java
@@ -0,0 +1,12 @@
+package org.software;
+
+import com.stacks.Bigcode;
+
+public class Main {
+    static final String MY_COUNTRY ="UGANDA";
+    public static void main(String[] args) {
+        Bigcode objs = new Bigcode();
+        objs.software();
+
+    }
+}
